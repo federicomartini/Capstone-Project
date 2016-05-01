356d02c79254ddbc9d01eaa9e82d31944515d3e8
Federico Martini <martini.f@gmail.com>
2016-05-01 19:58:21 +0200

    feat: Add Event Activity layout (not completed)

--- app/src/main/java/app/com/ttins/gettogether/common/EventActivity.java
+++ app/src/main/java/app/com/ttins/gettogether/common/EventActivity.java
@@ -1,5 +1,18 @@
 package app.com.ttins.gettogether.common;
 
 
-public class EventActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.com.ttins.gettogether.R;
+
+public class EventActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+
+    }
 }
