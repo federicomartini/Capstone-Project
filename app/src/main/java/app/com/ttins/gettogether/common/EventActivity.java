0854808226355cbaf67a406592c7dd1168c36b42
Federico Martini <martini.f@gmail.com>
2016-05-06 16:09:55 +0200

    Fix: Rename files

--- app/src/main/java/app/com/ttins/gettogether/common/EventActivity.java
+++ app/src/main/java/app/com/ttins/gettogether/common/EventActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import app.com.ttins.gettogether.R;
+import app.com.ttins.gettogether.eventlist.EventListView;
 
-public class EventActivity extends AppCompatActivity{
+public class EventActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        EventListView
 
 
     }
